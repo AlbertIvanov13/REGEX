fb544e2ba7a2e01454480e5525529ab24afc59cf
Albert Ivanov <albertivanov187@gmail.com>
2025-01-15 17:36:47 +0200

    100/100 :D

---
+++ 02. MatchPhoneNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Text.RegularExpressions;
+
+var regex = @"\+359([-\s])2\1(\d{3})\1(\d{4})\b";
+var input = Console.ReadLine();
+
+var matches = Regex.Matches(input, regex);
+
+var matchedPhones = matches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+Console.WriteLine(string.Join(", ", matchedPhones));
