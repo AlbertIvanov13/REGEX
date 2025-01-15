5b72f6827bf4f46a0b3bff414c10b379c52a9a25
Albert Ivanov <albertivanov187@gmail.com>
2025-01-15 21:56:54 +0200

    100/100

---
+++ 03. MatchDates/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Text.RegularExpressions;
+
+var regex = @"(?<day>\d{2})(\.|-|/)(?<month>[A-Z][a-z]+)\1(?<year>\d{4})";
+
+var input = Console.ReadLine();
+
+var dates = Regex.Matches(input, regex);
+
+foreach (Match date in dates)
+{
+	var day = date.Groups["day"].Value;
+	var month = date.Groups["month"].Value;
+	var year = date.Groups["year"].Value;
+
+    Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+}
