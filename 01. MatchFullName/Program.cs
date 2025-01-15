98a73df43c0d9143fa8c3dccd45ac8e8b3bc2d6f
Albert Ivanov <albertivanov187@gmail.com>
2025-01-15 13:48:56 +0200

    100/100

--- 01. MatchFullName/Program.cs
+++ 01. MatchFullName/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+using System.Text.RegularExpressions;
+
+string pattern = @"\b([A-Z][a-z])[a-z]* ([A-Z][a-z])[a-z]*\b";
+string names = Console.ReadLine();
+
+MatchCollection matchedNames = Regex.Matches(names, pattern);
+
+foreach (Match m in matchedNames)
+{
+    Console.Write(m.Value + " ");
+}
+
+Console.WriteLine();
