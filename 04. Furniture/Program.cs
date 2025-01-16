5c85cda052c83aaa0a64013b6ef841773d0d3f0b
Albert Ivanov <albertivanov187@gmail.com>
2025-01-16 16:44:20 +0200

    100/100 :D

--- 04. Furniture/Program.cs
+++ 04. Furniture/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace _04._Furniture
+﻿using System.Text.RegularExpressions;
+
+namespace _04._Furniture
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			string pattern = @"\>\>(?<type>[A-ZA-z]+)\<\<(?<price>\d+|\d+\.\d+)\!\d+";
+			string pattern = @"\>\>(?<type>[A-ZA-z]+)\<\<(?<price>\d+|\d+\.\d+)\!(?<quantity>\d+)";
+
+			List<Furniture> allFurnitures = new List<Furniture>();
+			decimal money = 0;
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input == "Purchase")
+				{
+					break;
+				}
+
+				Match furniture = Regex.Match(input, pattern);
+
+				if (furniture.Success)
+				{
+					Furniture newFurniture = new Furniture();
+					newFurniture.Name = furniture.Groups["type"].Value;
+					newFurniture.Price = decimal.Parse(furniture.Groups["price"].Value);
+					newFurniture.Quantity = int.Parse(furniture.Groups["quantity"].Value);
+					money += newFurniture.TotalMoney();
+
+					allFurnitures.Add(newFurniture);
+				}
+
+			}
+
+            Console.WriteLine("Bought furniture: ");
+
+			foreach (var item in allFurnitures)
+			{
+                Console.WriteLine($"{item.Name}");
+			}
+
+            Console.WriteLine($"Total money spend: {money:f2}");
 		}
 
 		class Furniture
@@ -14,9 +50,9 @@
             public decimal Price { get; set; }
             public int Quantity { get; set; }
 
-			public void TotalMoney()
+			public decimal TotalMoney()
 			{
-				Price *= Quantity;
+				return Price *= Quantity;
 			}
         }
 	}
