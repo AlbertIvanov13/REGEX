18de0d9e3d0a059fbbfb2f14b0a08bbdf45ed979
Albert Ivanov <albertivanov187@gmail.com>
2025-01-16 16:05:39 +0200

    Update

---
+++ 04. Furniture/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Furniture
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string pattern = @"\>\>(?<type>[A-ZA-z]+)\<\<(?<price>\d+|\d+\.\d+)\!\d+";
+		}
+
+		class Furniture
+		{
+            public string Name { get; set; }
+
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+			public void TotalMoney()
+			{
+				Price *= Quantity;
+			}
+        }
+	}
+}
