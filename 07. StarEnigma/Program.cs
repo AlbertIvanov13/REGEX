bd8ccc723e4250e44e29f4b456ded8f09aa182ca
Albert Ivanov <albertivanov187@gmail.com>
2025-01-19 23:43:37 +0200

    Update

--- 07. StarEnigma/Program.cs
+++ 07. StarEnigma/Program.cs
@@ -11,31 +11,29 @@ namespace _07._StarEnigma
 		static void Main(string[] args)
 		{
 			string pattern = @"[STARstar]";
-			string newPattern = @".*\@(?<planetName>[A-Za-z]+)[^\@\-\!\:\>]*:(?<population>\d+)[^\@\-\!\:\>]*!(?<attackType>A|D)![^\@\-\!\:\>]*->(?<soldierCount>\d+).*";
-			uint userInput; while (!uint.TryParse(Console.ReadLine(), out userInput) || userInput < 0 || userInput > 100) Environment.Exit(0);
+			string newPattern = @".*\@(?<planetName>[A-Z][a-z]+)[^\@\-\!\:\>]*\:(?<population>\d+)[^\@\-\!\:\>]*\!(?<attackType>[AD])[^\@\-\!\:\>]*\![^\@\-\!\:\>]*\->(?<soldierCount>\d+).*";
+			int userInput = int.Parse(Console.ReadLine());
 			uint count = 0;
 
 			List<Planet> attackedPlanets = new List<Planet>();
 			List<Planet> destroyedPlanets = new List<Planet>();
 
+			List<char> newStrings = new List<char>();
+
 			StringBuilder sb = new StringBuilder();
+			bool isContaining = false;
 			for (int i = 0; i < userInput; i++)
 			{
 				string encryptedMessage = Console.ReadLine();
 
-				if (encryptedMessage == null)
-				{
-					break;
-				}
-
 				Match match = Regex.Match(encryptedMessage, pattern);
-
 				if (match.Success)
 				{
 					count = 0;
 					sb = new StringBuilder();
 					for (int j = 0; j < encryptedMessage.Length; j++)
 					{
+						newStrings.Add(encryptedMessage[j]);
 						if (encryptedMessage[j].ToString().Equals("S", StringComparison.OrdinalIgnoreCase))
 						{
 							count++;
@@ -55,28 +53,48 @@ namespace _07._StarEnigma
 					}
 				}
 
-				for (int k = 0; k < encryptedMessage.Length; k++)
-				{
-					sb.Append((char)(encryptedMessage[k] - count));
-				}
-
+					for (int k = 0; k < encryptedMessage.Length; k++)
+					{
+						sb.Append((char)(encryptedMessage[k] - count));
+					}
+			
 				string word = sb.ToString();
 
 				Match newMatch = Regex.Match(word, newPattern);
 				if (newMatch.Success)
 				{
 					string planetName = newMatch.Groups["planetName"].Value;
-					int population = int.Parse(newMatch.Groups["population"].Value);
+					uint population = uint.Parse(newMatch.Groups["population"].Value);
 					string attackType = newMatch.Groups["attackType"].Value;
-					int soldierCount = int.Parse(newMatch.Groups["soldierCount"].Value);
+					uint soldierCount = uint.Parse(newMatch.Groups["soldierCount"].Value);
 					Planet planet = new Planet(planetName, population, attackType, soldierCount);
 					if (attackType == "A")
 					{
-						attackedPlanets.Add(planet);
+						foreach (Planet item in attackedPlanets)
+						{
+							if (item.PlanetName == planetName)
+							{
+								isContaining = true;
+							}
+						}
+						if (!isContaining)
+						{
+							attackedPlanets.Add(planet);
+						}
 					}
 					else if (attackType == "D")
 					{
-						destroyedPlanets.Add(planet);
+						foreach (Planet item in destroyedPlanets)
+						{
+							if (item.PlanetName == planetName)
+							{
+								isContaining = true;
+							}
+						}
+						if (!isContaining)
+						{
+							destroyedPlanets.Add(planet);
+						}
 					}
 				}
 			}
@@ -98,12 +116,12 @@ namespace _07._StarEnigma
 
 		public class Planet
 		{
-			public string? PlanetName { get; set; }
-			public int Population { get; set; }
-			public string? AttackType { get; set; }
-			public int SoldierCount { get; set; }
+			public string PlanetName { get; set; }
+			public uint Population { get; set; }
+			public string AttackType { get; set; }
+			public uint SoldierCount { get; set; }
 
-            public Planet(string name, int population, string attackType, int soldierCount)
+            public Planet(string name, uint population, string attackType, uint soldierCount)
             {
 				PlanetName = name;
 				Population = population;
